b3dd81e25bbfc0457a75887d684edcd30da53ede
maxvyr <maxvid30@gmail.com>
2022-03-23 19:23:07 +0100

    :sparkles: add page scan QrCode

---
+++ lib/view/main_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:read_write_qr_code/utils/utils_navigator.dart';
+import 'package:read_write_qr_code/utils/utils_widget.dart';
+import 'package:read_write_qr_code/view/qr_code_generate_view.dart';
+import 'package:read_write_qr_code/view/qr_code_scan_view.dart';
+
+class MainView extends ConsumerStatefulWidget {
+  const MainView({Key? key}) : super(key: key);
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends ConsumerState<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return BaseWidget(
+      child: Expanded(
+        child: Center(
+            child: Row(
+          children: [
+            ElevatedButton(
+              onPressed: () => UtilsNavigator.goToNextPage(
+                context,
+                const QrCodeScanView(),
+              ),
+              child: const Text(
+                "Scan QrCode",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => UtilsNavigator.goToNextPage(
+                context,
+                const QrCodeGenerateView(),
+              ),
+              child: const Text(
+                "Generate QrCode",
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
---
+++ lib/view/qr_code_scan_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:read_write_qr_code/utils/utils_widget.dart';
+
+class QrCodeScanView extends StatelessWidget {
+  const QrCodeScanView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const BaseWidget(
+      child: Center(
+        child: Text("Coming Soon"),
+      ),
+    );
+  }
+}
